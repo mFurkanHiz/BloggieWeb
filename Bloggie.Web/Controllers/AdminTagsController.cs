077565de3e64b87f4a701af0e832c958635f35a3
mFurkanHiz <mf.wissen@gmail.com>
2023-04-05 15:00:01 +0300

    AdminTags controllerı eklendi içine Add actionu eklendi. add actionu için bir view oluşturuldu. Bu view Layout içinde çağırıldı.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        // Yapmış olduğumuz MVC routing şekli şunun gibi olaaktır.
+        // https://localhost:1544/AdminTags/Add
+        // …./controller/action
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
