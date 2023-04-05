b606bf2d83cd9afe0f0af6d4a1a8b539095225c8
mFurkanHiz <mf.wissen@gmail.com>
2023-04-05 16:46:29 +0300

    Add view'ı oluşturuldu bu view üzerinden post metodu tanımlandı. viewdeki veriler controllera aktarıldı

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -7,9 +7,23 @@ namespace Bloggie.Web.Controllers
         // Yapmış olduğumuz MVC routing şekli şunun gibi olaaktır.
         // https://localhost:1544/AdminTags/Add
         // …./controller/action
+        [HttpGet]
         public IActionResult Add()
         {
+
             return View();
+        }
+        // Http Post metodu çalıştırıyoruz. Burada amacımız Add View'ında oluşturmuş olduğumuz form içerisinde gerçekleşen POST metodu butona tıklandığında submit olsun ve bu submit sonrasında aşağıdaki actionumuz devreye girsin.
+        // peki kullanıcının bu bahsi geçen view üzerinde inputlara girmiş olduğu verileri sayfamıza nasıl çağırırız?
+        // 1- Bunun için önce view a gideceğiz ve inputlar abirer name vereceğiz. Sonra da controllerların içerisinde birer değişkende formdan gelen inputları aşağıdaki gibi tutacağız. Fakat get metodu kullandığımız add actionu ile post metodunda add actionunun adları aynı olamamaktadır ünkü bir metot aynı sayıda parametreyi alarak yeniden aynı isimde kullanılamamaktadır. dolayısıyla biz de metodumuzun adının farklı olmasını fakat davranış biçiinin aynı olmasını sağlamak adına action aname adında bir özellik kullanarak bu metodun da add actionu içinde davranış sergilemesini sağladık.
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            // submit işlemi gerçekleştikten sonra bizi Add.cshtml de tutmasını istiyoruz
+            return View("Add");
         }
     }
 }
