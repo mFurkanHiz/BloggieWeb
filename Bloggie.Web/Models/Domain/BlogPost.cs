6c348f3398e6576b3b66de030f0ea9535f1cbcf7
mFurkanHiz <mf.wissen@gmail.com>
2023-04-05 13:49:31 +0300

    Tag domain model eklendi. navigation propertyler tanımlandı

--- Bloggie.Web/Models/Domain/BlogPost.cs
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -3,7 +3,8 @@
     public class BlogPost
     {
         public Guid Id { get; set; }
-        public string Heading { get; set; }
+        public string Heading { get; set; } // property yi nullable bırakmak istersek türün sonuna (string in sonuna) soru işareti koyulur
+        //public string? Heading { get; set; } // now this is nullable
         public string PageTitle { get; set; }
         public string Content { get; set; }
         public string ShorstDescription { get; set; }
@@ -12,6 +13,7 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set;}
         public bool Visible { get; set;}
+        public ICollection<Tag> Tags { get; set;}
 
     }
 }
